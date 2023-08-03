9df4a40a561e78fe904f3d2f91633c2fe9730898
sushilkawad <sushilkawad@gmail.com>
2023-08-03 17:13:31 +0530

    Update RoutingHolder.js

--- src/RoutingHolder.js
+++ src/RoutingHolder.js
@@ -1,11 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
 import CartPage from './containers/CartPage';
 import HomePage from './containers/HomePage';
+import QR from './containers/QR';
 
 function RoutingHolder() {
 
   return (
       <Switch>
+        <Route path="/qr">
+          <QR />
+        </Route>
         <Route path="/cart">
           <CartPage />
         </Route>
